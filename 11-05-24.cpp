f665d1289942928f241a3a0118cf62a244ede859
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-05-11 18:04:14 +0530

    Create 11-05-24.cpp
    Juggler Sequence

---
+++ 11-05-24.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    vector<long long> jugglerSequence(long long n) {
+        vector<long long> ans;
+        ans.push_back(n);
+        while(n!=1 ){
+            if(n%2==0){
+                n=sqrt(n);
+                ans.push_back(n);
+            }
+            else{
+                n=sqrt(n)*sqrt(n)*sqrt(n);
+                ans.push_back(n);
+            }
+        }
+        return ans;
+    }
+};
+
