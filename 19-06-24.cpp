f0af9f0378b033ad8b86ba461afa9c39cc06ab0a
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-06-19 16:53:46 +0530

    Create 19-06-24.cpp
    Find maximum volume of a cuboid

---
+++ 19-06-24.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    double maxVolume(double perimeter, double area) {
+        // code here
+        
+        double p=perimeter,a=area;
+        double l=(p/2-sqrt(p*p/4-6*a))/6;
+        return l*l*(p/4-2*l);
+    }
+};
