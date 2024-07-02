217aeb0bed24a2cd4cc53d9026ea55ca698ec518
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-07-02 21:12:22 +0530

    Create 02-07-24.cpp
    Linked list of strings forms a palindrome

---
+++ 02-07-24.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool compute(Node* head) {
+        // Your code goes here
+        string s="";
+        Node* temp=head;
+        while(temp!=NULL)
+        {
+            s+=temp->data;
+            temp=temp->next;
+        }
+        int i=0,j=s.length()-1;
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+            return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
