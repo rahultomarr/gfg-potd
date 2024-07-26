d06353d4537425ccbdf29507985c232acff34e6d
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-07-26 20:27:57 +0530

    Create 26-07-24..cpp
    K-Pangrams

---
+++ 26-07-24..cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+    vector<int>arr(26);
+        for (char ch: str){
+            if (ch == ' ') continue;
+            arr[ch - 'a']+=1; 
+        }
+        int nhi_hai = 0;
+        int space_for_alpha = 0; // matlab eesa character jo phele bhi aa chuka hai or uski jagah apan eesa characters likhe jo phele nhi aaya
+        for (int i=0; i< 26; i++){
+            if (arr[i] > 1) space_for_alpha += (arr[i]-1);
+            if (!arr[i]) nhi_hai+=1; // sentance me eesa characters jo 26 characters me se nhi aaya
+        }
+        
+        return nhi_hai <= space_for_alpha && nhi_hai <= k;
+    }
+};
