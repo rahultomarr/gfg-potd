3a05295c0547964c8e0d6cf86562875af0b8346f
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-08-11 13:54:11 +0530

    Create 11-08-24.cpp
    Job Sequencing Problem

---
+++ 11-08-24.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+    public:
+    //Function to find the maximum profit and the number of jobs done.
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        // your code here
+     set<int> slots;
+        for(int i = 1; i <= n; i++)
+            slots.insert(i);
+        sort(arr, arr + n, [](Job& j1, Job&j2) {
+           return j1.profit > j2.profit; 
+        });
+        
+        int cnt = 0, profit = 0;
+        for(int i = 0; i < n; i++) {
+            Job curr = arr[i];
+            auto it = slots.upper_bound(curr.dead);
+            if(it == slots.begin()) continue;
+            it = prev(it);
+            cnt++;
+            profit += curr.profit;
+            slots.erase(it);
+        }
+        
+        return {cnt, profit};
+    }  
+};
