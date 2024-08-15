f10b9510e8859b331250a70a0099332219e9bc37
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-08-15 16:43:29 +0530

    Create 15-08-24.cpp
    Add 1 to a Linked List Number

---
+++ 15-08-24.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    Node* addOne(Node* head) {
+        // Your Code here
+  // return head of list after adding one
+        Node* temp = head;
+        Node* change = NULL;
+        while(temp){
+            Node* prev = temp;
+            if(temp->next == NULL){
+                if(temp->data < 9){
+                    temp->data += 1;
+                    return head;
+                }
+                break;
+            }
+            temp = temp->next;
+            if(prev->data!=9 && temp && temp->data==9){
+                change = prev;
+            }
+        }
+        if(change==NULL){
+            Node* first = new Node(1);
+            first->next = head;
+            while(head){
+                head->data = 0;
+                head= head->next;
+            }
+            return first;
+        }
+        else{
+            change->data += 1;
+            change = change->next;
+            while(change){
+                change->data = 0;
+                change= change->next;
+            }
+            return head;
+        }
+        return head;
+    }
+};
