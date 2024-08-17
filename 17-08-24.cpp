cdc1ae23d562e6268f551de8863a176feb0d8955
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-08-17 13:25:40 +0530

    Create 17-08-24.cpp
    Product array puzzle

---
+++ 17-08-24.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    // nums: given vector
+    // return the Product vector P that hold product except self at each index
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+
+        // code here
+     vector<long long>ans(nums.size(), 1);
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = 0; j < nums.size(); j++)
+            {
+                if(j != i)
+                {
+                    ans[i] *= nums[j];
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
