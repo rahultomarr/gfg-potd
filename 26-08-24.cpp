edbd148d4169c3468dbbf004a1a96a441fceabea
rahul tomar <115893086+rahultomarr@users.noreply.github.com>
2024-08-26 11:29:55 +0530

    Create 26-08-24.cpp
    Wildcard Pattern Matching

---
+++ 26-08-24.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    /*You are required to complete this method*/
+    int wildCard(string pattern, string str) {
+        // code here
+    int n = pattern.size(), m = str.size(); int dp[n+2][m+2];
+        memset(dp, 0, sizeof(dp)); dp[n][m] = 1;
+        for ( int i = n; i>=0; i-- ){
+            for ( int j = m; j>=0; j-- ){
+                if ( pattern[i] == str[j] || pattern[i] == '?' ) {
+                    dp[i][j] |= dp[i+1][j+1];
+                }
+                if ( pattern[i] == '*' ){
+                    dp[i][j] |= dp[i+1][j+1];
+                    dp[i][j] |= dp[i][j+1];
+                    dp[i][j] |= dp[i+1][j];
+                } 
+            }
+        } return dp[0][0];
+    }
+};
